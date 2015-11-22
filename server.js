1fa1e309efaae345ea0fe89662e25da044a2aab9
Reuben Berghan <reuben.berghan@gmail.com>
2015-11-22 21:26:20 +1100

    Final statis todos

--- server.js
+++ server.js
@@ -14,13 +14,19 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=false
 app.get('/todos', function(req, res) {
-	var queryParams = _.pick(req.query, ['description','completed']),
+	var queryParams = _.pick(req.query, ['q','completed']),
 		filteredTodos = todos;
 	
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, { completed: true });
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, { completed: false });
+	}
+	
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 	
 	res.json(filteredTodos);
