721f520f119332abb5cb260c425f5a39fc287314
Reuben Berghan <reuben.berghan@gmail.com>
2015-12-01 07:18:53 +1100

    Add Postgres for production

--- db.js
+++ db.js
@@ -1,9 +1,18 @@
 var Sequelize = require('sequelize'),
+	env = process.env.NODE_ENV || 'development',
+	sequelize,
+	db = {};
+	
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		'dialect': 'postgres'
+	})
+} else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		'dialect': 'sqlite',
 		'storage': __dirname + '/data/dev-todo-api.sqlite'
-	}),
-	db = {};
+	})
+}
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
