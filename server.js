e50708e0c703e10f230f3d4bf33afa82437a12d5
Reuben Berghan <reuben.berghan@gmail.com>
2015-12-13 17:20:21 +1100

    Add force: true to Sequelize sync

--- server.js
+++ server.js
@@ -167,6 +167,6 @@ app.post('/users/login', function(req, res) {
 	
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
 	app.listen(PORT, function() { console.log('Express listening on port: ' + PORT) });
 });
