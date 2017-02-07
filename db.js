64709da5fa9ae4e89c3f3b945a1b927f6dbb15c9
Reuben Berghan <reuben.berghan@gmail.com>
2017-02-07 21:50:57 +1100

    Refactored Sequelize initialisation

--- db.js
+++ db.js
@@ -1,18 +1,10 @@
-var Sequelize = require('sequelize'),
-	env = process.env.NODE_ENV || 'development',
-	sequelize,
-	db = {};
-	
-if (env === 'production') {
-	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		'dialect': 'postgres'
-	})
-} else {
-	sequelize = new Sequelize(undefined, undefined, undefined, {
-		'dialect': 'sqlite',
-		'storage': __dirname + '/data/dev-todo-api.sqlite'
-	})
-}
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const prodSeqInit = [ process.env.DATABASE_URL, { 'dialect': 'postgres' } ]
+const devSeqInit = [ null, null, null, { 'dialect': 'sqlite', 'storage': __dirname + '/data/dev-todo-api.sqlite' } ]
+const seqInitArr = env === 'production' ? prodSeqInit : devSeqInit;
+const sequelize = new Sequelize(...seqInitArr);
+const db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
