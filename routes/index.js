b378b9cf998887137a9252aa930c604357148a6b
Reuben Berghan <reuben.berghan@gmail.com>
2017-02-16 20:50:24 +1100

    Add comments and udpate .gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
+// init API router
 const routes = require('express').Router();
+
+// require top level routes
 const todos = require('./todos');
 
+// attach routes to router
 routes.use('/todos', todos);
 
 module.exports = routes;
--- routes/todos/index.js
+++ routes/todos/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+// init todos router
 const todos = require('express').Router();
 
+// require routes
 const get = require('./get');
 const getById = require('./getById');
 const create = require('./create');
 const deleteById = require('./deleteById');
 const updateById = require('./updateById');
 
+// attach routes to todos router
 todos.get('/', get);
 todos.get('/:id', getById);
 todos.post('/', create);
